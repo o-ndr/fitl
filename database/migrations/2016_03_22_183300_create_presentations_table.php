fad98b265640150ba4835c4ac6e7ecc87a809387
Oleksandr Pysaryuk <alexpysaryuk@gmail.com>
2016-03-22 14:47:36 -0400

    added a presentations database miration file

---
+++ database/migrations/2016_03_22_183300_create_presentations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePresentationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presentations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('presentation_title');
+            $table->text('synopsis');
+            $table->string('conference_track')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('presentations');
+    }
+}
