fb9acd6f7ca1832a3ce2fd0e4f39331aeaab6538
Oleksandr Pysaryuk <alexpysaryuk@gmail.com>
2016-03-22 16:01:49 -0400

    created presentationcontroller and show page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,6 +24,8 @@ Route::get('about', 'PageController@about');
 
 Route::get('contact', 'PageController@contact');
 
+Route::get('presentations/{presentation}', 'PresentationController@show');
+
 
 /*
 |--------------------------------------------------------------------------
---
+++ resources/views/presentations/show.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Show Presentation Page</title>
+	</head>
+	<body>
+		<h1>Show Presentation Page</h1>
+			<p>Coming soon...</p>
+	</body>
+</html>
